1d0247d6c76b5f738ec1c3ae55d617dc17d1b20b
Paul <97813342+Paul1404@users.noreply.github.com>
2023-08-17 20:52:19 +0200

    Add project files.

---
+++ TrikotOrderForm.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace SVU_Bestellungen
+{
+    partial class TrikotOrderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TrikotOrderForm));
+            this.comboBoxArticle = new System.Windows.Forms.ComboBox();
+            this.comboBoxSize = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtColor = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDownQuantity = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAddOrder = new System.Windows.Forms.Button();
+            this.dataGridViewOrders = new System.Windows.Forms.DataGridView();
+            this.btnSaveOrders = new System.Windows.Forms.Button();
+            this.btnSaveSummary = new System.Windows.Forms.Button();
+            this.btnOpenFolder = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuantity)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxArticle
+            // 
+            this.comboBoxArticle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBoxArticle.FormattingEnabled = true;
+            this.comboBoxArticle.Location = new System.Drawing.Point(12, 56);
+            this.comboBoxArticle.Name = "comboBoxArticle";
+            this.comboBoxArticle.Size = new System.Drawing.Size(227, 28);
+            this.comboBoxArticle.TabIndex = 0;
+            // 
+            // comboBoxSize
+            // 
+            this.comboBoxSize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.comboBoxSize.FormattingEnabled = true;
+            this.comboBoxSize.Location = new System.Drawing.Point(245, 56);
+            this.comboBoxSize.Name = "comboBoxSize";
+            this.comboBoxSize.Size = new System.Drawing.Size(227, 28);
+            this.comboBoxSize.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Artikel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(241, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Größe";
+            // 
+            // txtColor
+            // 
+            this.txtColor.Location = new System.Drawing.Point(478, 58);
+            this.txtColor.Name = "txtColor";
+            this.txtColor.Size = new System.Drawing.Size(227, 26);
+            this.txtColor.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(474, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Farbe";
+            // 
+            // numericUpDownQuantity
+            // 
+            this.numericUpDownQuantity.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.numericUpDownQuantity.Location = new System.Drawing.Point(711, 58);
+            this.numericUpDownQuantity.Name = "numericUpDownQuantity";
+            this.numericUpDownQuantity.Size = new System.Drawing.Size(120, 26);
+            this.numericUpDownQuantity.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(707, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Menge";
+            // 
+            // btnAddOrder
+            // 
+            this.btnAddOrder.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddOrder.Location = new System.Drawing.Point(611, 121);
+            this.btnAddOrder.Name = "btnAddOrder";
+            this.btnAddOrder.Size = new System.Drawing.Size(587, 55);
+            this.btnAddOrder.TabIndex = 8;
+            this.btnAddOrder.Text = "Bestellung hinzufügen";
+            this.btnAddOrder.UseVisualStyleBackColor = true;
+            this.btnAddOrder.Click += new System.EventHandler(this.BtnAddOrder_Click);
+            // 
+            // dataGridViewOrders
+            // 
+            this.dataGridViewOrders.AllowUserToOrderColumns = true;
+            this.dataGridViewOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewOrders.Location = new System.Drawing.Point(12, 253);
+            this.dataGridViewOrders.Name = "dataGridViewOrders";
+            this.dataGridViewOrders.RowHeadersWidth = 62;
+            this.dataGridViewOrders.RowTemplate.Height = 28;
+            this.dataGridViewOrders.Size = new System.Drawing.Size(1186, 900);
+            this.dataGridViewOrders.TabIndex = 9;
+            // 
+            // btnSaveOrders
+            // 
+            this.btnSaveOrders.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSaveOrders.Location = new System.Drawing.Point(249, 121);
+            this.btnSaveOrders.Name = "btnSaveOrders";
+            this.btnSaveOrders.Size = new System.Drawing.Size(223, 55);
+            this.btnSaveOrders.TabIndex = 10;
+            this.btnSaveOrders.Text = "Speichern";
+            this.btnSaveOrders.UseVisualStyleBackColor = true;
+            this.btnSaveOrders.Click += new System.EventHandler(this.BtnSaveOrders_Click);
+            // 
+            // btnSaveSummary
+            // 
+            this.btnSaveSummary.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSaveSummary.Location = new System.Drawing.Point(12, 121);
+            this.btnSaveSummary.Name = "btnSaveSummary";
+            this.btnSaveSummary.Size = new System.Drawing.Size(223, 55);
+            this.btnSaveSummary.TabIndex = 11;
+            this.btnSaveSummary.Text = "Auswerten";
+            this.btnSaveSummary.UseVisualStyleBackColor = true;
+            // 
+            // btnOpenFolder
+            // 
+            this.btnOpenFolder.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOpenFolder.Location = new System.Drawing.Point(12, 182);
+            this.btnOpenFolder.Name = "btnOpenFolder";
+            this.btnOpenFolder.Size = new System.Drawing.Size(223, 55);
+            this.btnOpenFolder.TabIndex = 12;
+            this.btnOpenFolder.Text = "Zur Datei";
+            this.btnOpenFolder.UseVisualStyleBackColor = true;
+            // 
+            // TrikotOrderForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 1168);
+            this.Controls.Add(this.btnOpenFolder);
+            this.Controls.Add(this.btnSaveSummary);
+            this.Controls.Add(this.btnSaveOrders);
+            this.Controls.Add(this.dataGridViewOrders);
+            this.Controls.Add(this.btnAddOrder);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDownQuantity);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtColor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxSize);
+            this.Controls.Add(this.comboBoxArticle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TrikotOrderForm";
+            this.ShowIcon = false;
+            this.Text = "Bestellungs-Oberfläche";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownQuantity)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewOrders)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxArticle;
+        private System.Windows.Forms.ComboBox comboBoxSize;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtColor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDownQuantity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAddOrder;
+        private System.Windows.Forms.DataGridView dataGridViewOrders;
+        private System.Windows.Forms.Button btnSaveOrders;
+        private System.Windows.Forms.Button btnSaveSummary;
+        private System.Windows.Forms.Button btnOpenFolder;
+    }
+}
+
---
+++ TrikotOrderForm.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace SVU_Bestellungen
+{
+    public partial class TrikotOrderForm : Form
+    {
+        private DataTable ordersTable;
+
+        public TrikotOrderForm()
+        {
+            InitializeComponent();
+            InitializeOrdersTable();
+            InitializeControls();
+            LoadOrdersFromCSV();
+            btnSaveSummary.Click += (s, e) => EvaluateOrderQuantities();
+            this.AcceptButton = btnAddOrder;
+            // this.BackgroundImage = new Bitmap(Assembly.GetExecutingAssembly().GetManifestResourceStream("SVU_Bestellungen.background.jpg"));
+            dataGridViewOrders.BorderStyle = BorderStyle.None;
+            dataGridViewOrders.VirtualMode = true;
+            dataGridViewOrders.ScrollBars = ScrollBars.Vertical;
+            foreach (DataGridViewColumn column in dataGridViewOrders.Columns)
+            {
+                column.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+            this.BackColor = System.Drawing.Color.White;
+            this.ForeColor = System.Drawing.Color.DarkRed;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void LoadOrdersFromCSV()
+        {
+            if (File.Exists("bestellungen.csv"))
+            {
+                using (StreamReader sr = new StreamReader("bestellungen.csv", Encoding.UTF8))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] values = line.Split(',');
+                        if (values.Length == 4) // Ensure that there are 4 columns in the CSV
+                        {
+                            DataRow row = ordersTable.NewRow();
+                            row["Artikel"] = values[0];
+                            row["Größe"] = values[1];
+                            row["Farbe"] = values[2];
+                            row["Menge"] = values[3];
+                            ordersTable.Rows.Add(row);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void InitializeOrdersTable()
+        {
+            ordersTable = new DataTable();
+            ordersTable.Columns.Add("Artikel");
+            ordersTable.Columns.Add("Größe");
+            ordersTable.Columns.Add("Farbe");
+            ordersTable.Columns.Add("Menge");
+
+            dataGridViewOrders.DataSource = ordersTable;
+        }
+
+        private void InitializeControls()
+        {
+            comboBoxArticle.Items.Add("Trikot");
+            comboBoxArticle.Items.Add("Polo");
+
+            comboBoxSize.Items.Add("S");
+            comboBoxSize.Items.Add("M");
+            comboBoxSize.Items.Add("L");
+            comboBoxSize.Items.Add("XL");
+
+            // Styling
+            foreach (Control ctrl in this.Controls)
+            {
+                // Default Control Styling
+                ctrl.ForeColor = System.Drawing.Color.DarkRed;
+                ctrl.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular);
+
+                // Specific Control Styling
+                if (ctrl is Button)
+                {
+                    var button = ctrl as Button;
+                    button.BackColor = System.Drawing.Color.DarkRed;
+                    button.ForeColor = System.Drawing.Color.White;
+                    button.FlatStyle = FlatStyle.Flat;
+                    button.FlatAppearance.BorderColor = System.Drawing.Color.DarkRed;
+                    button.FlatAppearance.BorderSize = 1;
+                }
+                else if (ctrl is ComboBox)
+                {
+                    var comboBox = ctrl as ComboBox;
+                    comboBox.DropDownStyle = ComboBoxStyle.DropDownList;
+                    comboBox.FlatStyle = FlatStyle.Flat;
+                }
+                else if (ctrl is DataGridView)
+                {
+                    var dgv = ctrl as DataGridView;
+                    dgv.BackgroundColor = System.Drawing.Color.White;
+                    dgv.BorderStyle = BorderStyle.None;
+                    dgv.GridColor = System.Drawing.Color.DarkRed;
+                    dgv.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.LightCoral;
+                    dgv.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.White;
+                    dgv.RowHeadersVisible = false;
+                    dgv.ColumnHeadersDefaultCellStyle.BackColor = System.Drawing.Color.DarkRed;
+                    dgv.ColumnHeadersDefaultCellStyle.ForeColor = System.Drawing.Color.White;
+                    dgv.EnableHeadersVisualStyles = false;
+                }
+                else if (ctrl is TextBox)
+                {
+                    var txt = ctrl as TextBox;
+                    txt.BorderStyle = BorderStyle.FixedSingle;
+                }
+            }
+
+            // Button Hover Effects
+            foreach (Control ctrl in this.Controls)
+            {
+                if (ctrl is Button)
+                {
+                    var button = ctrl as Button;
+                    button.MouseEnter += (s, e) =>
+                    {
+                        button.BackColor = System.Drawing.Color.White;
+                        button.ForeColor = System.Drawing.Color.DarkRed;
+                        button.FlatAppearance.BorderColor = System.Drawing.Color.White;
+                    };
+                    button.MouseLeave += (s, e) =>
+                    {
+                        button.BackColor = System.Drawing.Color.DarkRed;
+                        button.ForeColor = System.Drawing.Color.White;
+                    };
+                }
+            }
+        }
+
+        private void BtnAddOrder_Click(object sender, EventArgs e)
+        {
+            DataRow row = ordersTable.NewRow();
+            row["Artikel"] = comboBoxArticle.SelectedItem?.ToString() ?? string.Empty;
+            row["Größe"] = comboBoxSize.SelectedItem?.ToString() ?? string.Empty;
+            row["Farbe"] = txtColor.Text;
+            row["Menge"] = numericUpDownQuantity.Value.ToString();
+            ordersTable.Rows.Add(row);
+        }
+
+        private void BtnSaveOrders_Click(object sender, EventArgs e)
+        {
+            using (StreamWriter sw = new StreamWriter("bestellungen.csv", false, Encoding.UTF8))
+            {
+                foreach (DataRow row in ordersTable.Rows)
+                {
+                    sw.WriteLine(string.Join(",", row.ItemArray));
+                }
+            }
+            MessageBox.Show("Bestellungen gespeichert!");
+        }
+
+        private void EvaluateOrderQuantities()
+        {
+            // Key: Artikel|Größe|Farbe, Value: Gesamtmenge
+            Dictionary<string, int> summary = new Dictionary<string, int>();
+
+            foreach (DataRow row in ordersTable.Rows)
+            {
+                string article = row["Artikel"].ToString();
+                string size = row["Größe"].ToString();
+                string color = row["Farbe"].ToString();
+                int quantity = int.Parse(row["Menge"].ToString());
+
+                string key = $"{article}|{size}|{color}";
+
+                if (summary.ContainsKey(key))
+                {
+                    summary[key] += quantity;
+                }
+                else
+                {
+                    summary[key] = quantity;
+                }
+            }
+
+            // Speichern der zusammengefassten Daten in einer CSV-Datei
+            using (StreamWriter sw = new StreamWriter("bestellzusammenfassung.csv", false, Encoding.UTF8))
+            {
+                // Header
+                sw.WriteLine("Artikel,Größe,Farbe,Menge");
+
+                foreach (var entry in summary)
+                {
+                    string[] parts = entry.Key.Split('|');
+                    sw.WriteLine($"{parts[0]},{parts[1]},{parts[2]},{entry.Value}");
+                }
+            }
+
+            // Anzeige der zusammengefassten Daten
+            StringBuilder orderSummary = new StringBuilder("Bestellzusammenfassung:\n\n");
+            foreach (var entry in summary)
+            {
+                orderSummary.AppendLine($"{entry.Key.Replace("|", " - ")}: {entry.Value} Stück");
+            }
+
+            MessageBox.Show(orderSummary.ToString());
+            MessageBox.Show("Bestellzusammenfassung erfolgreich im CSV-Format gespeichert!");
+        }
+
+        private void btnOpenFolder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get the directory of the current executable
+                string directoryPath = Path.GetDirectoryName(Application.ExecutablePath);
+
+                // Check if directoryPath is not null, otherwise provide a fallback value
+                string safeDirectoryPath = directoryPath ?? string.Empty;
+
+                // Open the directory in the default file explorer
+                if (!string.IsNullOrEmpty(safeDirectoryPath))
+                {
+                    Process.Start("explorer.exe", safeDirectoryPath);
+                }
+                else
+                {
+                    MessageBox.Show("Konnte den Ordner nicht finden.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Display a user-friendly message
+                MessageBox.Show($"Ein Fehler ist aufgetreten: {ex.Message}");
+            }
+        }
+
+        private void DataGridViewOrders_Paint(object sender, PaintEventArgs e)
+        {
+            Rectangle borderRectangle = dataGridViewOrders.Bounds;
+            borderRectangle.Inflate(-1, -1);  // Optionally shrink the rectangle slightly if you want the border inside the DGV's bounds
+            ControlPaint.DrawBorder(e.Graphics, borderRectangle, Color.Red, ButtonBorderStyle.Solid);
+        }
+    }
+}

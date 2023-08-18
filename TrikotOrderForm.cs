5e374348da2f35ddf311a685c3ecf574e9c38f2f
Paul <97813342+Paul1404@users.noreply.github.com>
2023-08-18 22:38:30 +0200

    Full Documentation

--- TrikotOrderForm.cs
+++ TrikotOrderForm.cs
@@ -40,12 +40,43 @@ namespace SVU_Bestellungen
             this.Load += TrikotOrderForm_Load;  // Registering the Load event
         }
 
+
+        /// <summary>
+        /// Handles the "Load" event of the TrikotOrderForm. Sets initial control states and focus.
+        /// </summary>
+        /// <param name="sender">The source of the event. Typically, the instance of TrikotOrderForm that triggered the event.</param>
+        /// <param name="e">Event data. Contains no specific data for this event.</param>
+        /// <remarks>
+        /// This method performs the following initializations:
+        ///
+        /// 1. Sets the focus to the 'txtNachname' textbox, allowing users to start inputting data immediately upon form load.
+        ///
+        /// 2. Initializes the 'numericUpDownQuantity' control value to 1.
+        ///
+        /// It ensures a smooth user experience by priming controls for immediate use once the form loads.
+        /// </remarks>
         private void TrikotOrderForm_Load(object sender, EventArgs e)
         {
             txtNachname.Focus();
             numericUpDownQuantity.Value = 1;
         }
 
+
+        /// <summary>
+        /// Logs an informational message to the 'richTextBoxLogs' with a timestamp and an 'INFO' indication.
+        /// </summary>
+        /// <param name="message">The message to be logged.</param>
+        /// <remarks>
+        /// This method performs the following operations:
+        ///
+        /// 1. Generates a timestamp based on the current date and time in the format "dd/MM/yyyy HH:mm:ss".
+        ///
+        /// 2. Appends the message with a preceding 'INFO' indication and timestamp to the 'richTextBoxLogs'.
+        ///
+        /// 3. Automatically scrolls to the end of the 'richTextBoxLogs' to ensure the latest log is visible.
+        ///
+        /// This method provides a consistent way of logging informational messages in the application.
+        /// </remarks>
         private void LogMessage(string message)
         {
             string timestamp = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
@@ -53,6 +84,28 @@ namespace SVU_Bestellungen
             richTextBoxLogs.ScrollToCaret(); // Scroll to the end of the logs
         }
 
+
+        /// <summary>
+        /// Logs an error message to the 'richTextBoxLogs' with a timestamp and error indication.
+        /// </summary>
+        /// <param name="message">The error message to be logged.</param>
+        /// <remarks>
+        /// This method performs the following operations:
+        ///
+        /// 1. Generates a timestamp based on the current date and time in the format "dd/MM/yyyy HH:mm:ss".
+        ///
+        /// 2. Adjusts the selection in the 'richTextBoxLogs' to the end, ensuring that new logs are appended.
+        ///
+        /// 3. Changes the text color to red to indicate an error message.
+        ///
+        /// 4. Appends the error message with a preceding error indication and timestamp.
+        ///
+        /// 5. Resets the text color back to the default for subsequent logs.
+        ///
+        /// 6. Automatically scrolls to the end of the 'richTextBoxLogs' to ensure the latest log is visible.
+        ///
+        /// The method is useful for providing consistent error logging in the application with visual distinction for errors.
+        /// </remarks>
         private void ErrorMessage(string message)
         {
             string timestamp = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
@@ -93,6 +146,22 @@ namespace SVU_Bestellungen
         //    }
         //}
 
+
+        /// <summary>
+        /// Loads order records from the SQLite database and populates the ordersTable.
+        /// </summary>
+        /// <remarks>
+        /// This method connects to the SQLite database 'bestellungen.db' and fetches all the records from the 'Orders' table.
+        /// Each record is then converted into a DataRow and added to the ordersTable.
+        /// 
+        /// Assumptions:
+        /// - The SQLite database has been properly initialized and contains a table named 'Orders'.
+        /// - The 'Orders' table contains columns: 'Nachname', 'Vorname', 'Größe', 'Initialen', and 'Menge'.
+        /// - The ordersTable DataTable has corresponding columns to hold the data.
+        /// - The 'Menge' column in the SQLite table contains integers.
+        /// 
+        /// Note: Exception handling for database access or data conversion errors has not been implemented in this method.
+        /// </remarks>
         private void LoadOrdersFromSQLite()
         {
             using (SQLiteConnection conn = new SQLiteConnection("data source=bestellungen.db"))
@@ -119,7 +188,21 @@ namespace SVU_Bestellungen
         }
 
 
-
+        /// <summary>
+        /// Initializes the ordersTable with appropriate columns and binds it to the dataGridViewOrders control.
+        /// </summary>
+        /// <remarks>
+        /// This method sets up the structure for the ordersTable DataTable by defining the required columns:
+        /// - 'Nachname'
+        /// - 'Vorname'
+        /// - 'Größe'
+        /// - 'Initialen'
+        /// - 'Menge' (with a data type of integer)
+        /// 
+        /// After defining the columns, the method then sets the dataGridViewOrders control's DataSource property to this ordersTable, allowing the control to display the table's data.
+        /// 
+        /// Note: It's assumed that the dataGridViewOrders control has been properly initialized prior to calling this method.
+        /// </remarks>
         private void InitializeOrdersTable()
         {
             ordersTable = new DataTable();
@@ -133,6 +216,25 @@ namespace SVU_Bestellungen
         }
 
 
+        /// <summary>
+        /// Initializes the SQLite database for storing orders. If the database file does not exist, it is created.
+        /// </summary>
+        /// <remarks>
+        /// This method checks if the 'bestellungen.db' file exists in the application's directory.
+        /// If it doesn't exist, the method creates a new SQLite database file with that name.
+        ///
+        /// Once the database file is ensured to exist, the method establishes a connection to the database 
+        /// and checks for the existence of the 'Orders' table. If the table doesn't exist, it is created 
+        /// with the following columns:
+        /// - 'Nachname' (Text type)
+        /// - 'Vorname' (Text type)
+        /// - 'Größe' (Text type)
+        /// - 'Initialen' (Text type)
+        /// - 'Menge' (Integer type)
+        ///
+        /// This method should be called during the initialization phase of the application or before 
+        /// any database operations to ensure the database structure is in place.
+        /// </remarks>
         private void InitializeDatabase()
         {
             if (!File.Exists("bestellungen.db"))
@@ -154,6 +256,26 @@ namespace SVU_Bestellungen
         }
 
 
+        /// <summary>
+        /// Initializes and styles the controls present on the form.
+        /// </summary>
+        /// <remarks>
+        /// This method performs the following operations:
+        /// 
+        /// 1. Populates the 'comboBoxSize' control with the size options ("S", "M", "L", "XL").
+        ///
+        /// 2. Applies default styling (color and font) to all controls on the form.
+        /// 
+        /// 3. Applies specific styling to controls based on their type:
+        ///    - For Button controls: Sets the background, foreground color, border style, etc.
+        ///    - For ComboBox controls: Sets the dropdown style.
+        ///    - For DataGridView controls: Sets the background, border style, grid color, row headers, column header styles, etc.
+        ///    - Additional controls can have specific styling added in this section.
+        ///
+        /// 4. Applies hover effects to Button controls, changing their appearance when the mouse cursor enters and leaves the button area.
+        /// 
+        /// Ensure that this method is called during form initialization, after control properties have been set and before the form is displayed.
+        /// </remarks>
         private void InitializeControls()
         {
             comboBoxSize.Items.Add("S");
@@ -223,6 +345,25 @@ namespace SVU_Bestellungen
             }
         }
 
+
+        /// <summary>
+        /// Saves the provided order data to the SQLite database.
+        /// </summary>
+        /// <param name="order">The DataRow representing the order to be saved.</param>
+        /// <remarks>
+        /// This method performs the following operations:
+        /// 
+        /// 1. Establishes a connection with the SQLite database 'bestellungen.db'.
+        ///
+        /// 2. Constructs an INSERT SQL statement to add the order data to the 'Orders' table in the database.
+        /// 
+        /// 3. Binds the values from the provided DataRow 'order' to the SQL statement parameters.
+        ///
+        /// 4. Executes the SQL statement, thereby saving the order to the database.
+        ///
+        /// Ensure that the DataRow 'order' passed to this method contains the necessary columns 
+        /// (Nachname, Vorname, Größe, Initialen, Menge) and that the database connection string is correctly configured.
+        /// </remarks>
         private void SaveOrderToSQLite(DataRow order)
         {
             using (SQLiteConnection conn = new SQLiteConnection("data source=bestellungen.db"))
@@ -244,6 +385,26 @@ namespace SVU_Bestellungen
         }
 
 
+
+        /// <summary>
+        /// Event handler for the "Add Order" button click.
+        /// </summary>
+        /// <param name="sender">The source of the event, typically the button itself.</param>
+        /// <param name="e">Details about the click event.</param>
+        /// <remarks>
+        /// This method performs the following operations upon button click:
+        /// 
+        /// 1. Creates a new DataRow 'row' and populates it with data from the UI controls (textboxes, combobox, etc.).
+        ///
+        /// 2. Adds the populated 'row' to the orders DataTable 'ordersTable'.
+        ///
+        /// 3. Invokes the <see cref="SaveOrderToSQLite"/> method to save the order details to the SQLite database.
+        ///
+        /// 4. Clears all the input controls in preparation for a new entry.
+        ///
+        /// Ensure that all the required UI controls are appropriately initialized and accessible. 
+        /// This method assumes that 'ordersTable' is properly instantiated.
+        /// </remarks>
         private void BtnAddOrder_Click(object sender, EventArgs e)
         {
             DataRow row = ordersTable.NewRow();
@@ -266,7 +427,24 @@ namespace SVU_Bestellungen
         }
 
 
-
+        /// <summary>
+        /// Event handler for the "Save Orders" button click.
+        /// </summary>
+        /// <param name="sender">The source of the event, typically the button itself.</param>
+        /// <param name="e">Details about the click event.</param>
+        /// <remarks>
+        /// This method performs the following operations upon button click:
+        /// 
+        /// 1. Initiates a StreamWriter to write to the 'bestellungen.csv' file with UTF8 encoding.
+        ///
+        /// 2. Iterates through each DataRow in 'ordersTable' and writes the order details as comma-separated values to the CSV file.
+        ///
+        /// 3. Closes the StreamWriter upon completion.
+        ///
+        /// 4. Calls the <see cref="LogMessage"/> method to log the success message.
+        ///
+        /// Note: It's advisable to handle potential exceptions that might arise, especially when working with file I/O.
+        /// </remarks>
         private void BtnSaveOrders_Click(object sender, EventArgs e)
         {
             using (StreamWriter sw = new StreamWriter("bestellungen.csv", false, Encoding.UTF8))
@@ -280,6 +458,25 @@ namespace SVU_Bestellungen
             LogMessage("Bestellungen gespeichert!");
         }
 
+
+        /// <summary>
+        /// Evaluates and summarizes order quantities based on sizes.
+        /// </summary>
+        /// <remarks>
+        /// This method performs the following operations:
+        /// 
+        /// 1. Iterates through each DataRow in 'ordersTable' and constructs a summary of total quantities ordered for each size.
+        ///
+        /// 2. Writes the summarized order data to a CSV file named 'bestellzusammenfassung.csv' with UTF8 encoding.
+        ///
+        /// 3. The CSV file will contain two columns: 'Größe' (Size) and 'Menge' (Quantity).
+        ///
+        /// 4. Constructs a string representation of the order summary and logs it using the <see cref="LogMessage"/> method.
+        ///
+        /// 5. Logs a success message indicating that the order summary has been saved successfully in CSV format.
+        ///
+        /// Note: It's advisable to handle potential exceptions that might arise, especially when working with file I/O or data manipulation.
+        /// </remarks>
         private void EvaluateOrderQuantities()
         {
             // Key: Größe, Value: Gesamtmenge
@@ -324,6 +521,22 @@ namespace SVU_Bestellungen
         }
 
 
+        /// <summary>
+        /// Handles the click event of the 'btnOpenFolder' button. Opens the folder containing the application executable in the default file explorer.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        /// <remarks>
+        /// This method performs the following operations:
+        ///
+        /// 1. Tries to determine the directory in which the current executable resides using the <see cref="Application.ExecutablePath"/>.
+        ///
+        /// 2. If successful, opens the determined directory in the default file explorer using the explorer.exe command.
+        ///
+        /// 3. In case of any issues (like a null directory path or an exception during the process launch), provides feedback to the user through log messages or message boxes.
+        ///
+        /// It is recommended to handle potential exceptions that might arise when dealing with file paths and launching external processes.
+        /// </remarks>
         private void btnOpenFolder_Click(object sender, EventArgs e)
         {
             try
@@ -351,6 +564,23 @@ namespace SVU_Bestellungen
             }
         }
 
+
+        /// <summary>
+        /// Handles the Paint event of the 'dataGridViewOrders' control. Draws a custom border around the DataGridView.
+        /// </summary>
+        /// <param name="sender">The source of the event, typically the DataGridView itself.</param>
+        /// <param name="e">The <see cref="PaintEventArgs"/> instance containing the event data, including a reference to the graphics used for painting.</param>
+        /// <remarks>
+        /// This method performs the following operations:
+        ///
+        /// 1. Determines the boundary of the DataGridView using its Bounds property.
+        ///
+        /// 2. Optionally, it adjusts (shrinks) the boundary rectangle slightly for visual purposes using the Inflate method.
+        ///
+        /// 3. Uses the ControlPaint utility's DrawBorder method to paint a solid red border around the adjusted boundary of the DataGridView.
+        ///
+        /// This is particularly useful when custom visual effects are required for the DataGridView that aren't provided by default properties and methods.
+        /// </remarks>
         private void DataGridViewOrders_Paint(object sender, PaintEventArgs e)
         {
             Rectangle borderRectangle = dataGridViewOrders.Bounds;
